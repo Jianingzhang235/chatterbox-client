cd847790fed84f7762c779e7b01a2bde281deb7b
Jianing Zhang <jianingzhang082@gmail.com>
2019-04-20 18:16:46 -0700

    fix the url of GET

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -22,7 +22,7 @@ var Parse = {
 
   readAll: function(successCB, errorCB = null) {
     $.ajax({
-  url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+  url: Parse.server,
       type: 'GET',
       data: { order: '-createdAt' },
       contentType: 'application/json',
